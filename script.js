117b39e5a40796278ad223801265ea8e852eab0f
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-23 01:47:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 //your code here!
 // Function to add 2 more list items to the end of the list
+const list = document.getElementById("infi-list");
+let i =0;
 function addItemsToList() {
-    const list = document.getElementById("list");
-
-    // Add 2 more list items
-    for (let i = 0; i < 2; i++) {
-        const newItem = document.createElement("li");
-        newItem.textContent = `Item ${list.children.length + 1}`;
-        list.appendChild(newItem);
-    }
+	    const li = document.createElement("li");
+	    li.innerText = `Item ${++i}`;
+		list.appendChild(li);
 }
+
 
 // Add about 10 list items by default
-for (let i = 1; i <=10; i++) {
-    const newItem = document.createElement("li");
-    newItem.textContent = `Item ${i}`;
-    document.getElementById("list").appendChild(newItem);
+for (let i = 0; i <10; i++) {
+    addItemsToList();
 }
 
-// Attach a scroll event listener to the window
-window.addEventListener("scroll", function () {
-    const list = document.getElementById("list");
+list.addEventListener("scroll", () => {
+	let maxScroll = list.scrollHeight - list.clientHeight;
+	let currentScrollTop = list.scrollTop;
 
-    // Check if the user has reached the end of the list
-    if (window.innerHeight + window.scrollY >= list.clientHeight + list.offsetTop) {
-        // Add 2 more list items when the user reaches the end
-        addItemsToList();
-    }
+	let thresold = Math.abs(maxScroll-currentScrollTop);
+	if(thresold<=5){
+		for (let i = 0; i < 2; i++) {
+			addItemsToList();
+		}
+	}
 });
-
